6d6ea6f35835306c0f7a32d307c2e136f06c5a81
ejahdev <116228748+ejahdev@users.noreply.github.com>
2023-10-10 13:43:32 -0700

    Add files via upload

--- js/countdownPopup.js
+++ js/countdownPopup.js
@@ -9,47 +9,89 @@ function updateClock() {
     clockElement.textContent = `Current Time: ${hours}:${minutes}:${seconds}`;
 }
 
+// Call updateClock() every second
+setInterval(updateClock, 1000);
+
+
 // Function to update the countdown and handle the popup
 function updateCountdown() {
     const now = new Date();
     const minutes = now.getMinutes();
     const seconds = now.getSeconds();
 
+    console.log(`Minutes: ${minutes}, Seconds: ${seconds}`); // Log the current time
+
     if (minutes === 15 && seconds === 0) {
+        console.log('Calling showPopup()'); // Log when showPopup() is called
         showPopup(); // Call the function to show the popup
     } else if (minutes === 16 && seconds === 0) {
+        console.log('Calling hidePopup()'); // Log when hidePopup() is called
         hidePopup(); // Call the function to hide the popup
     } else {
         hideCountdown();
     }
 }
 
+
+
 // Function to show the popup
 function showPopup() {
-    console.log('showPopup() called');
-    const popupElement = document.getElementById('popup');
-    popupElement.style.display = 'block';
+    try {
+        console.log('showPopup() called');
+        const popupElement = document.getElementById('popup');
 
-    // Determine the direction of the cloud animation (left or right)
-    const direction = Math.random() < 0.5 ? 'slide-in-left' : 'slide-in-right';
-    popupElement.classList.add(direction);
+        if (!popupElement) {
+            throw new Error('Popup element not found');
+        }
 
-    setTimeout(function () {
-        document.getElementById('popup-text').textContent = 'Cheers! Happy 20!!';
-    }, 500); // Delay showing text for half a second
+        popupElement.style.display = 'block';
 
-    setTimeout(function () {
-        document.getElementById('popup-text').style.opacity = '1';
-    }, 1000); // Show text after 1 second
+        // Determine the direction of the cloud animation (left or right)
+        const direction = Math.random() < 0.5 ? 'slide-in-left' : 'slide-in-right';
+        popupElement.classList.add(direction);
+
+        setTimeout(function () {
+            const popupTextElement = document.getElementById('popup-text');
+            if (!popupTextElement) {
+                throw new Error('Popup text element not found');
+            }
+
+            popupTextElement.textContent = '5 Minutes till the 20!!';
+        }, 500); // Delay showing text for half a second
+
+        setTimeout(function () {
+            const popupTextElement = document.getElementById('popup-text');
+            if (!popupTextElement) {
+                throw new Error('Popup text element not found');
+            }
+
+            popupTextElement.style.opacity = '1';
+        }, 1000); // Show text after 1 second
+
+        // Hide the popup after 1 minute (60000 milliseconds)
+        setTimeout(function () {
+            hidePopup();
+        }, 60000);
+    } catch (error) {
+        console.error('Error in showPopup():', error);
+    }
 }
 
 // Function to hide the popup
 function hidePopup() {
     const popupElement = document.getElementById('popup');
-    popupElement.style.display = 'none';
-    document.getElementById('popup-text').textContent = '';
-    document.getElementById('popup-text').style.opacity = '0';
-    popupElement.classList.remove('slide-in-left', 'slide-in-right'); // Remove animation classes
+    const popupTextElement = document.getElementById('popup-text');
+
+    // Add the moveDown animation
+    popupElement.style.animation = 'moveDown 5s ease-in-out forwards';
+
+    setTimeout(function () {
+        // Hide the popup and reset animation
+        popupElement.style.display = 'none';
+        popupElement.style.animation = '';
+        popupTextElement.textContent = '';
+        popupTextElement.style.opacity = '0';
+    }, 5000); // Delay for 5 seconds to match the moveDown animation duration
 }
 
 // Function to hide the countdown
@@ -59,9 +101,7 @@ function hideCountdown() {
 
     // Check for the countdown and clock again in 1 second
     setTimeout(updateCountdown, 1000);
-    setTimeout(updateClock, 1000);
 }
 
 // Initial check for the countdown and clock
 updateCountdown();
-updateClock();
