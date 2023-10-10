1c9d2c60c7492d2cf8d335b34476c602e51380fe
ejahdev <116228748+ejahdev@users.noreply.github.com>
2023-10-10 13:48:30 -0700

    Add files via upload

--- js/countdownPopup.js
+++ js/countdownPopup.js
@@ -1,7 +1,13 @@
 // Function to update the clock
 function updateClock() {
     const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0');
+    const hours24 = now.getHours();
+    let hours12 = hours24;
+    if (hours24 > 12) {
+        hours12 = hours24 - 12;
+    }
+    
+    const hours = hours12.toString().padStart(2, '0');
     const minutes = now.getMinutes().toString().padStart(2, '0');
     const seconds = now.getSeconds().toString().padStart(2, '0');
 
