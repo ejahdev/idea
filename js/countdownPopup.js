661bd0466bde804118c866f624b69a85e07744b1
ejahdev <116228748+ejahdev@users.noreply.github.com>
2023-10-11 03:24:35 -0700

    Add files via upload

--- js/countdownPopup.js
+++ js/countdownPopup.js
@@ -13,17 +13,18 @@ function showMessage(message, duration) {
 
     if (message === '5 minutes till the 20!') {
         showClock(); // Show the clock when the first message appears
-    } else if (message === 'CHEERS HAPPY 20!!') {
-        messageShown = true; // Mark the message as shown
-        finalMessageTimeout = setTimeout(() => {
-            hideMessage(); // Hide the final message after 1 minute
-            stopCloudsAnimation(); // Stop cloud animation when the final message leaves
-        }, 60000);
     }
 
     setTimeout(() => {
-        if (message !== 'CHEERS HAPPY 20!!') {
-            hideMessage(); // Hide all other messages
+        countdownMessage.style.display = 'none';
+        countdownMessage.style.animation = 'none';
+
+        if (message === 'CHEERS HAPPY 20!!') {
+            messageShown = true; // Mark the message as shown
+            finalMessageTimeout = setTimeout(() => {
+                hideClouds(); // Hide the clouds 1 minute after the final message
+                hideMessage(); // Hide the final message after 1 minute
+            }, 60000);
         }
     }, duration);
 }
@@ -41,23 +42,27 @@ function updateClock() {
     const now = new Date();
     const hours = now.getHours();
     const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
     const ampm = hours >= 12 ? 'PM' : 'AM';
     const twelveHourFormat = hours % 12 || 12; // Convert to 12-hour format
 
-    const timeString = `${twelveHourFormat}:${minutes.toString().padStart(2, '0')} ${ampm}`;
+    const timeString = `${twelveHourFormat}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')} ${ampm}`;
     clock.innerText = timeString;
 
     setTimeout(updateClock, 1000); // Update the clock every second
 }
 
-function stopCloudsAnimation() {
-    cloudLeft.style.animation = 'none';
-    cloudRight.style.animation = 'none';
+function hideClouds() {
+    cloudLeft.style.display = 'none';
+    cloudRight.style.display = 'none';
 }
 
 function hideMessage() {
-    countdownMessage.style.display = 'none';
-    countdownMessage.style.animation = 'none';
+    if (!messageShown) {
+        // Ensure the final message is hidden only once
+        countdownMessage.style.display = 'none';
+        countdownMessage.style.animation = 'none';
+    }
 }
 
 function updateCountdown() {
